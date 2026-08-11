738b93c8c5e4734c0a25149718e27304b8f588fd
Denis Dreval <dreval@narod.ru>
2026-08-11 22:25:08 +0300

    Add readiness/liveness probes

--- services/app/src/main/java/dev/trainground/app/Application.java
+++ services/app/src/main/java/dev/trainground/app/Application.java
@@ -2,10 +2,26 @@ package dev.trainground.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+    }
+}
+
+@Component
+class ChaosMonkey {
+    @Scheduled(fixedRate = 30000)
+    void maybeDie() {
+        if (ThreadLocalRandom.current().nextInt(10) == 0) {
+            System.out.println("ChaosMonkeyL simulating crash now");
+            System.exit(1);
+        }
     }
 }
